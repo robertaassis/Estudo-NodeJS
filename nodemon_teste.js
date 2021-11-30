3d9710449983e4bc1c6841af86cf42ba1ee39824
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2021-11-30 14:24:22 -0300

    Nodemon

--- nodemon_teste.js
+++ nodemon_teste.js
@@ -1,3 +1,3 @@
-// nao precisa ficar instanciando o arquivo toda hora; qualquer alteraçao feita nesse arquivo, ele ja roda
+// nao precisa ficar instanciando o arquivo a qualquer momento; qualquer alteraçao feita nesse arquivo, ele ja roda
 const carro = 'bmw'
 console.log('carro e ' + carro)
