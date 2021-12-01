ba0d8299d22bfc318783a44d512825e9d95c8b19
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2021-12-01 11:58:04 -0300

    Instalação (arquivos enormes) e utilização de framework Express

---
+++ exp/app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+
+const adminRoutes = require('./routes/admin')
+
+const cookieParser = require('cookie-parser') // middleware de terceiro; tem que instalar
+
+app.use(cookieParser())
+
+app.use(express.json()) // aceita padrao json
+
+app.use( (req, res, next) => { // middleware entra no meio de uma requisição e trata a informação; quando termina de fazer o que ele tem que fazer nessa função, ele chama a função next
+    console.log("Executando middleware em nivel de aplicação") // vai printar toda hora que alguem entrar em qualquer pagina
+    return next()
+})
+
+app.get('/setcookie', (req, res) =>{
+    res.cookie('user', 'Roberta Assis', {maxAge:90000, httpOnly:true}) // gero o cookie 
+    return res.send('Cookie gravado com sucesso')
+})
+
+app.get('/getcookie', (req, res) =>{
+    let user = req.cookies['user'] // pega o user do cookie; f12 > application > cookies > value
+    if(user) res.send(user)
+})
+
+app.get('/', (req,res) => { // primeira pagina
+    res.send('Vai dar certo')
+}) 
+
+app.get('*', (req,res,next) => { // primeira pagina
+    setImmediate(()=> {
+        next(new Error('Temos um problema!!')) // leva esse erro para a proxima função, que no caso eh 
+    // console.log('Erro gerado') // printa isso no console
+    // res.status(500).json({message: err.message}) 
+    })
+}) 
+
+app.use((err,req,res,next)=>{ // tratamento de erro
+    console.log('Erro gerado') // printa isso no console
+    res.status(500).json({message: err.message}) // printa isso na tela 
+})
+
+app.use('/admin', adminRoutes) // se alguem chamar o /admin, vai pras rotas (arquivo) do admin.js
+
+app.listen(3000, ()=>{
+    console.log('Server running!')
+})
---
+++ exp/routes/admin.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+
+// NAO PRECISA COLOCAR /admin PQ SO VAI ACESSAR ESSA ROTA SE TIVER ACESSANDO O /admin
+router.use(express.json()) // aceita padrao json
+
+function logReq(req,res,next){
+    if(1<0)  console.log("deu erro")
+    else return next() // ele só vai para a proxima (next; printar Listando os adm) se o que ele estiver (1 for maior que 0) pra fazer der certo aqui
+}
+// Ex: antes de printar Listando os adm, eu quero ir no banco checar se aquele adm existe, logo posso fazer com if, e 
+// caso exista mesmo, darei return next() para puxar o res.send da proxima(next) funcao
+router.get('/', logReq, (req,res) => { // primeira pagina; // printa quando alguem entra na pagina principal
+    res.send('Listando os adm')
+}) 
+
+// post eh pra envio de informações
+router.post('/', (req,res) => { // pagina admin
+    const corpo = `Login: ${req.body.login}\nSenha: ${req.body.senha}`
+    res.send('Administração via post! ' + corpo)
+})
+
+// path eh pra alterar alguns dados, put eh pra todos
+router.patch('/:id', (req,res) => { // pagina admin
+    res.send('Administração via patch do usuario ' +req.params.id)
+})
+
+router.put('/:id', (req,res) => { // pagina admin
+    res.send('Administração via put id: '+req.params.id)
+})
+
+
+router.delete('/:id', (req,res) => { // pagina admin
+    res.send('Administração via delete id: '+req.params.id)
+})
+
+router.get('/:id', (req,res) => { // pagina admin
+    res.send(`Administração com o id: ${req.params.id}!`)
+})
+
+module.exports = router
